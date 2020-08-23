df611a4dec57eebe2cbaa1452eab9ce5a1c8edce
louis-richard <louisr@irfu.se>
2020-08-23 14:08:45 +0200

    syntax update

--- pyrfu/pyrf/convert_fac.py
+++ pyrfu/pyrf/convert_fac.py
@@ -5,7 +5,7 @@ from .resample import resample
 from .ts_vec_xyz import ts_vec_xyz
 from .calc_fs import calc_fs
 
-def convert_fac(inp=None, Bbgd=None, r=np.array([1,0,0])):
+def convert_fac(inp=None, b_bgd=None, r=np.array([1,0,0])):
 	"""
 	Transforms to a field-aligned coordinate (FAC) system defined as:
 	R_parallel_z aligned with the background magnetic field
@@ -28,6 +28,7 @@ def convert_fac(inp=None, Bbgd=None, r=np.array([1,0,0])):
 			Time series of the input field in field aligned coordinates system
 
 	Example :
+		>>> from pyrfu import mms, pyrf
 		>>> # Time interval
 		>>> Tint = ["2019-09-14T07:54:00.000","2019-09-14T08:11:00.000"]
 		>>> # Spacecraft index
@@ -43,48 +44,57 @@ def convert_fac(inp=None, Bbgd=None, r=np.array([1,0,0])):
 	
 	"""
 	
-	if (inp is None) or (Bbgd is None):
+	if (inp is None) or (b_bgd is None):
 		raise ValueError("convert_fac requires at least 2 arguments")
 
-	if not isinstance(inp,xr.DataArray):
+	if not isinstance(inp, xr.DataArray):
 		raise TypeError("inp must be a DataArray")
 
-	if not isinstance(Bbgd,xr.DataArray):
+	if not isinstance(b_bgd, xr.DataArray):
 		raise TypeError("Bbgd must be a DataArray")
 
-	if len(inp) != len(Bbgd):
-		#Bbgd = resample(Bbgd,inp)	# issues with frequency estimation of Bscm
-		Bbgd = resample(Bbgd,inp,fs=calc_fs(inp))
+	if len(inp) != len(b_bgd):
+		b_bgd = resample(b_bgd, inp, fs=calc_fs(inp))
 	
-	t           = inp.time
-	inp_data    = inp.data
-	Bbgd        = Bbgd
-	Bn          = Bbgd/np.linalg.norm(Bbgd,axis=1,keepdims=True)
+	t 			= inp.time
+	inp_data 	= inp.data
+	b_bgd 		= b_bgd
+	bn 			= b_bgd/np.linalg.norm(b_bgd, axis=1, keepdims=True)
 
-	if isinstance(r,(list,np.ndarray)) and len(r) == 3:
-		r = np.tile(r,(len(Bbgd),1))
+	if isinstance(r, (list, np.ndarray)) and len(r) == 3:
+		r = np.tile(r, (len(b_bgd), 1))
 	elif isinstance(r, xr.DataArray):
-		r = resample(r,Bbgd)
-	
-	Rpar    = Bn
-	Rperpy  = np.cross(Rpar,r,axis=1)
-	Rperpy  = Rperpy/np.linalg.norm(Rperpy,axis=1,keepdims=True)
-	Rperpx  = np.cross(Rperpy,Bbgd,axis=1)
-	Rperpx  = Rperpx/np.linalg.norm(Rperpx,axis=1,keepdims=True)
+		r = resample(r, b_bgd)
+	else:
+		raise TypeError("Invalid type of spacecraft position")
+
+	# Parallel
+	r_par = bn
+
+	# Perpandicular
+	r_perp_y = np.cross(r_par, r, axis=1)
+	r_perp_y /= np.linalg.norm(r_perp_y, axis=1, keepdims=True)
+	r_perp_x = np.cross(r_perp_y, b_bgd, axis=1)
+	r_perp_x /= np.linalg.norm(r_perp_x, axis=1, keepdims=True)
 	
 	(ndata, ndim) = inp_data.shape
 	
 	if ndim == 3:
-		outdata         = np.zeros(inp.shape)
-		outdata[:,2]    = Rpar[:,0]*inp_data[:,0]+  Rpar[:,1]*inp_data[:,1]+  Rpar[:,2]*inp_data[:,2]
-		outdata[:,0]    = Rperpx[:,0]*inp_data[:,0]+Rperpx[:,1]*inp_data[:,1]+Rperpx[:,2]*inp_data[:,2]
-		outdata[:,1]    = Rperpy[:,0]*inp_data[:,0]+Rperpy[:,1]*inp_data[:,1]+Rperpy[:,2]*inp_data[:,2]
-		out             = xr.DataArray(outdata,coords=[inp.time.data,inp.comp],dims=["time","comp"])
+		outdata = np.zeros(inp.shape)
 
-	elif ndim ==1:
-		outdata         = np.zeros((2,ndata))
-		outdata[:,0]    = inp[:,0]*(Rperpx[:,0]*r[:,0] + Rperpx[:,1]*r[:,1] + Rperpx[:,2]*r[:,2])
-		outdata[:,1]    = inp[:,0]*(Rpar[:,0]*r[:,0]    + Rpar[:,1]*r[:,1]   + Rpar[:,2]*r[:,2]) 
-		out             = ts_vec_xy(inp.time.data,outdata,attrs=inp.attrs)
+		outdata[:, 0] = np.sum(r_perp_x * inp_data, axis=1)
+		outdata[:, 1] = np.sum(r_perp_y * inp_data, axis=1)
+		outdata[:, 2] = np.sum(r_par * inp_data, axis=1)
+
+		# xarray
+		out = xr.DataArray(outdata, coords=[inp.time.data, inp.comp], dims=["time", "comp"])
+
+	elif ndim == 1:
+		outdata = np.zeros((2, ndata))
+
+		outdata[:, 0] = inp[:, 0]*(r_perp_x[:, 0] * r[:, 0] + r_perp_x[:, 1] * r[:, 1] + r_perp_x[:, 2] * r[:, 2])
+		outdata[:, 1] = inp[:, 0]*(r_par[:, 0] * r[:, 0] + r_par[:, 1] * r[:, 1] + r_par[:, 2] * r[:, 2])
+
+		out = ts_vec_xy(inp.time.data, outdata, attrs=inp.attrs)
 
 	return out
