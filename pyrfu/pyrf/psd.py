039d96531b6ee77145a9a68a6a209a6fc8941826
louis-richard <louisr@irfu.se>
2020-08-26 13:55:44 +0200

    syntax update

--- pyrfu/pyrf/psd.py
+++ pyrfu/pyrf/psd.py
@@ -2,13 +2,9 @@ import numpy as np
 import xarray as xr
 import warnings
 from scipy import signal
-from astropy.time import Time
-import pyfftw
-import multiprocessing as mp
 
 
-
-def psd(inp=None, nfft=256, noverlap=128, window="hamming", dflag="constant", scalling="density"):
+def psd(inp=None, nfft=256, n_overlap=128, window="hamming", dflag="constant", scalling="density"):
 	"""
 	Estimate power spectral density using Welch's method.
 	
@@ -47,113 +43,28 @@ def psd(inp=None, nfft=256, noverlap=128, window="hamming", dflag="constant", sc
 		- [2] :     M.S. Bartlett, "Periodogram Analysis and Continuous Spectra", Biometrika, vol. 37, pp. 1-16, 1950.
 
 	"""
-	if not isinstance(inp,xr.DataArray):
+	if not isinstance(inp, xr.DataArray):
 		raise TypeError("inp must be a DataArray")
 
 	if inp.ndim == 2 and inp.shape[-1] == 3:
-		inp = abs(inp)
+		inp = np.abs(inp)
 
-	if not noverlap is None:
-		nperseg = 2*noverlap
-	else :
-		nperseg     = 256
-		noverlap    = nperseg/2
+	if n_overlap is None:
+		n_persegs = 256
+		n_overlap = n_persegs / 2
+	else:
+		n_persegs = 2 * n_overlap
 
+	if nfft < n_persegs:
+		nfft = n_persegs
+		warnings.warn("nfft < n_persegs. set to n_persegs", UserWarning)
 
-	if nfft < nperseg:
-		nfft = nperseg
-		warnings.warn("nfft < nperseg. set to nperseg",UserWarning)
+	dt = np.median(np.diff(inp.time.data)).astype(float) * 1e-9
+	fs = 1 / dt
 
-	dt = np.median(np.diff(inp.time.data)).astype(float)*1e-9
-	Fs = 1/dt
+	f, p_xx = signal.welch(inp.data, nfft=nfft, fs=fs, window=window, noverlap=n_overlap, detrend=dflag, \
+							nperseg=n_persegs, scaling=scalling, return_onesided=True, axis=-1)
 
-	f, Pxx = signal.welch(inp.data, nfft=nfft, fs=Fs, window=window, noverlap=noverlap, detrend=dflag, \
-							nperseg=nperseg, scaling=scalling, return_onesided=True, axis=-1)
+	out = xr.DataArray(p_xx, coords=[f], dims=["f"])
 
-	out = xr.DataArray(Pxx,coords=[f],dims=["f"])
-
-	return out
-#-----------------------------------------------------------------------------------------------------------------------
-def psd_m(inp=None, comp="", window="hanning", noverlap=0, nfft=256, dflag=None):
-	"""
-	Estimates the Power Spectral Density of signal vector X using Welch's averaged periodogram method.  X is divided
-	into overlapping sections, each of which is detrended, then windowed by the WINDOW parameter, then zero-padded to 
-	length NFFT. The magnitude squared of the length NFFT DFTs of the sections are averaged to form Pxx.  Pxx is length 
-	NFFT/2+1 for NFFT even, (NFFT+1)/2 for NFFT odd, or NFFT if the signal X is complex.  If you specify a scalar for
-	WINDOW, a Hanning window of that length is used.  Fs is the sampling frequency which doesn't affect the spectrum 
-	estimate but is used for scaling of plots.  See Page 556, A.V. Oppenheim and R.W. Schafer, Digital Signal 
-	Processing, Prentice-Hall, 1975.
-
-	Parameters :
-		- inp               [xarray]                Input time serie
-		- comp              [str]                   Target component. If not given (default) compute power spectral 
-													density of the magnitude
-		- window            [str]                   Type of window applied hannng (default), etc.
-		- noverlap          [int]                   Number of samples of overlapping of the sections of inp
-		- nfft              [int]                   Number of frequencies of fft
-		- dflag             [str]                   Detrending mode for the prewindowed sections of inp. 
-													None (default), linear or mean
-	
-	Returns :
-		- out               [xarray]                Power Spectrum of the input
-	"""
-
-	if not isinstance(inp,xr.DataArray):
-		raise TypeError("Input must be a DataArray")
-
-	t   = Time(inp.time.data,format="datetime64").unix
-	N   = len(inp)
-	dt  = np.median(np.diff(t))
-	fs  = 1/dt
-
-	if comp:
-		x = inp.sel(comp=comp).data
-	else :
-		x = abs(inp).data
-
-
-	window = eval("np."+window+"(nfft)")
-
-	n       = len(x)
-	nwind   = len(window)
-
-	k = np.fix((n-noverlap)/(nwind-noverlap)).astype(int)
-
-	index = np.arange(nwind)
-
-	KMU = k*np.linalg.norm(window)**2
-
-	Spec = np.zeros(nfft)
-	for i in range(k):
-		if dflag == None:
-			xw = window*(x[index])
-		elif isinstance(dflag,str) and dflag == "linear":
-			xw = window*signal.detrend(x[index],type='linear')
-		else :
-			xw = window*signal.detrend(x[index],type='constant')
-
-		index   = index + (nwind - noverlap)
-		Xx      = np.abs(pyfftw.interfaces.numpy_fft.fft(xw,nfft,axis=0,threads=mp.cpu_count()))**2
-		Spec    = Spec + Xx
-
-	if not any(np.imag(x) != 0):
-		if np.remainder(nfft,2):
-			select = np.arange((nff+1)/2)
-		else :
-			select = np.arange(nfft/2+1).astype(int)
-		Spec = Spec[select]
-	else :
-		select = np.arange(nfft)
-
-	freq_vector = select*fs/nfft
-
-	# I hate decibells, added to make correct units - AV 97.12.20
-	Spec=Spec/fs*2
-	#confid=confid/Fs*2;
-	#
-	# original line
-	Spec = Spec*(1/KMU)   # normalize
-	f = freq_vector
-
-	out = xr.DataArray(Spec,coords=[f],dims="freq")
 	return out
--- pyrfu/pyrf/resample.py
+++ pyrfu/pyrf/resample.py
@@ -6,8 +6,7 @@ import xarray as xr
 
 
 
-
-def resample(inp=None,ref=None,**kwargs):
+def resample(inp=None, ref=None, **kwargs):
 	"""
 	Resample inp to the time line of ref. If sampling of X is more than two times higher than Y, we average X, otherwise
 	we interpolate X.
@@ -59,135 +58,140 @@ def resample(inp=None,ref=None,**kwargs):
 		
 	"""
 
-	haveoptions = False
-
-	if not isinstance(inp,xr.DataArray):
+	if not isinstance(inp, xr.DataArray):
 		raise TypeError("Input must be a DataArray")
 
-	if not isinstance(ref,xr.DataArray):
+	if not isinstance(ref, xr.DataArray):
 		raise TypeError("Reference must be a DataArray")
 
-	if kwargs: have_options = True
+	sfy, thresh, method = [[], 0, ""]
 
-	sfy         = []
-	thresh      = 0
-	method      = ""
-	flag_do     = "check"
-	median_flag = False
-	mean_flag   = False
-	max_flag    = False
+	flag_do = "check"
 
-	#pdb.set_trace()
+	median_flag, mean_flag, max_flag = [False, False, False]
+
 	if "method" in kwargs:
-		if isinstance(kwargs["method"],str):
+		if isinstance(kwargs["method"], str):
 			method = kwargs["method"]
-		else :
+		else:
 			raise TypeError("METHOD must be string")
 
 	if "fs" in kwargs:
-		if sfy: raise ValueError("FSAMPLE/WINDOW already specified")
+		if sfy:
+			raise ValueError("fs/window already specified")
 
-		if isinstance(kwargs["fs"],int) or isinstance(kwargs["fs"],float):
+		if isinstance(kwargs["fs"], int) or isinstance(kwargs["fs"], float):
 			sfy = kwargs["fs"]
-		else :
-			raise TypeError("FSAMPLE must be numeric")
+		else:
+			raise TypeError("fs must be numeric")
 
 	if "window" in kwargs:
-		if sfy: raise ValueError("FSAMPLE/WINDOW already specified")
+		if sfy:
+			raise ValueError("fs/window already specified")
 
-		if (isinstance(kwargs["window"],int)
-				or isinstance(kwargs["window"],float)
-				or isinstance(kwargs["window"],np.ndarray)):
-			sfy = 1/kwargs["window"]
+		if (isinstance(kwargs["window"], int) or isinstance(kwargs["window"], float)
+				or isinstance(kwargs["window"], np.ndarray)):
+
+			sfy = 1 / kwargs["window"]
 		else:
 			raise TypeError("METHOD must be numeric")
 
 	if "mean" in kwargs:
 		if kwargs["mean"]:
-			mean_flag   = True
-			flag_do     = "average"
+			mean_flag = True
+
+			flag_do = "average"
 
 	if "median" in kwargs:
 		if kwargs["median"]:
 			median_flag = True
-			flag_do     = "average"
+
+			flag_do = "average"
 
 	if "max" in kwargs:
 		if kwargs["max"]:
-			max_flag    = True
-			flag_do     = "average"
+			max_flag = True
 
-	inp_time = inp.time.data.view("i8")*1e-9
-	ref_time = ref.time.data.view("i8")*1e-9
+			flag_do = "average"
+
+	inp_time = inp.time.data.view("i8") * 1e-9
+	ref_time = ref.time.data.view("i8") * 1e-9
 	inp_data = inp.data
 	
 	if len(inp) == 1:
 		if len(inp.shape) == 1:
-			outdata = np.tile(inp_data,len(ref_time))
-		else :
-			outdata = np.tile(inp_data,(len(ref_time),1))
+			outdata = np.tile(inp_data, len(ref_time))
+		else:
+			outdata = np.tile(inp_data, (len(ref_time), 1))
 
 	ndata = len(ref_time)
 
 	if flag_do == "check":
 		if ndata > 1:
 			if not sfy:
-				sfy1 = 1/(ref_time[1]-ref_time[0])
+				sfy1 = 1 / (ref_time[1] - ref_time[0])
 
 				if ndata == 2:
 					sfy = sfy1
+
+					not_found = False
 				else:
 					not_found = True
 
-				cur     = 2
-				MAXTRY  = 10
+				cur, max_try = [2, 10]
 
-				while (not_found and cur <= ndata and cur-3 < MAXTRY):
-					sfy = 1/(ref_time[cur]-ref_time[cur-1])
+				while not_found and cur <= ndata and cur - 3 < max_try:
+					sfy = 1 / (ref_time[cur] - ref_time[cur - 1])
 
 					if np.absolute(sfy-sfy1) < sfy*0.001:
-						not_found   = 0
-						sfy         = (sfy+sfy1)/2
+						not_found = False
+
+						sfy = (sfy + sfy1) / 2
+
 						break
 
 					sfy = sfy1
+
 					cur += 1
 
 				if not_found:
 					sfy = sfy1
-					raise RuntimeError("{Cannot guess sampling frequency. Tried {:d} times}".format(MAXTRY))
+
+					raise RuntimeError("Cannot guess sampling frequency. Tried {:d} times".format(max_try))
 				
 				del sfy1
 
 			if len(inp_time) / (inp_time[-1] - inp_time[0]) > 2 * sfy:
 				flag_do = "average"
-				warnings.warn("Using averages in resamp",UserWarning)
+				warnings.warn("Using averages in resamp", UserWarning)
 			else:
 				flag_do = "interpolation"
-		else :
+		else:
 			flag_do = "interpolation"  # If one output time then do interpolation
 
 	if flag_do == "average":
 		if method:
-			raise RunTimeError("cannot mix interpolation and averaging flags")
+			raise ValueError("cannot mix interpolation and averaging flags")
 
 		if not sfy:
-			sfy1 = 1/(ref_time[1]-ref_time[0])
+			sfy1 = 1 / (ref_time[1] - ref_time[0])
 
 			if ndata == 2:
 				sfy = sfy1
+
+				not_found = False
 			else:
 				not_found = True
 			
-			cur = 2
-			MAXTRY = 10
+			cur, max_try = [2, 10]
 			
-			while (not_found and cur <= ndata and cur-3 < MAXTRY):
-				sfy = 1/(ref_time[cur]-ref_time[cur-1])
+			while not_found and cur <= ndata and cur - 3 < max_try:
+				sfy = 1 / (ref_time[cur] - ref_time[cur - 1])
 				
-				if np.absolute(sfy-sfy1) < sfy*0.001:
-					not_found   = 0
-					sfy         = (sfy+sfy1)/2;
+				if np.absolute(sfy - sfy1) < sfy * .001:
+					not_found = False
+
+					sfy = (sfy + sfy1) / 2
 					break
 				
 				sfy = sfy1
@@ -195,70 +199,67 @@ def resample(inp=None,ref=None,**kwargs):
 
 			if not_found:
 				sfy = sfy1
-				raise RuntimeError("Cannot guess sampling frequency. Tried {:d} times".format(MAXTRY))
+				raise RuntimeError("Cannot guess sampling frequency. Tried {:d} times".format(max_try))
+
 			del sfy1
 
-		dt2 = 0.5 / sfy  # Half interval
+		dt2 = .5 / sfy  # Half interval
 
-		#pdb.set_trace()
 		inp_shape = list(inp_data.shape)
 		inp_shape[0] = ndata
 
 		inp_shape = tuple(inp_shape)
 
 		outdata = np.zeros(inp_shape)
-		"""
-		try:
-			outdata = np.zeros((ndata, inp_data.shape[1]))
-		except IndexError:
-			outdata = np.zeros(ndata)
-		"""
-		
+
 		for i in range(ndata):
-			idxl    = bisect.bisect_left(inp_time, ref_time[i] - dt2)
-			idxr    = bisect.bisect_right(inp_time, ref_time[i] + dt2)
-			ii      = np.arange(idxl, idxr)
+			idx_l = bisect.bisect_left(inp_time, ref_time[i] - dt2)
+			idx_r = bisect.bisect_right(inp_time, ref_time[i] + dt2)
+
+			ii = np.arange(idx_l, idx_r)
 
 			if ii.size == 0:
 				outdata[i, ...] = np.nan
 			else:
 				if thresh:
-					sdev    = np.std(inp_data[ii, ...])
-					mm      = np.mean(inp_data[ii, ...])
+					sdev = np.std(inp_data[ii, ...], axis=0)
 
-					if any(not np.isnan(sdev)):
+					mm = np.mean(inp_data[ii, ...], axis=0)
+
+					if any(np.isnan(sdev)):
 						for k in range(len(sdev)):
 							if not np.isnan(sdev[k]):
 								kk = bisect.bisect_right(inp_data[ii, k + 1] - mm[k], thresh * sdev[k])
 								if kk:
 									if median_flag:
-										outdata[j, k + 1] = np.median(inp_data[ii[kk], k + 1],axis=0)
+										outdata[i, k + 1] = np.median(inp_data[ii[kk], k + 1], axis=0)
 									elif max_flag:
-										outdata[j, k + 1] = np.max(inp_data[ii[kk], k + 1],axis=0)
+										outdata[i, k + 1] = np.max(inp_data[ii[kk], k + 1], axis=0)
 									else:
-										outdata[j, k + 1] = np.mean(inp_data[ii[kk], k + 1],axis=0)
+										outdata[i, k + 1] = np.mean(inp_data[ii[kk], k + 1], axis=0)
 								else:
 									outdata[i, k + 1] = np.nan
 							else:
 								outdata[i, ...] = np.nan
 				else:
 					if median_flag:
-						outdata[i, ...] = np.median(inp_data[ii, ...],axis=0)
+						outdata[i, ...] = np.median(inp_data[ii, ...], axis=0)
 					elif max_flag:
-						outdata[i, ...] = np.max(inp_data[ii, ...],axis=0)
+						outdata[i, ...] = np.max(inp_data[ii, ...], axis=0)
 					else:
-						outdata[i, ...] = np.mean(inp_data[ii, ...],axis=0)
+						outdata[i, ...] = np.mean(inp_data[ii, ...], axis=0)
 
 	elif flag_do == "interpolation":
-		if any([mean_flag,median_flag,max_flag]):
-			raise RunTimeError("cannot mix interpolation and averaging flags")
+		if any([mean_flag, median_flag, max_flag]):
+			raise ValueError("cannot mix interpolation and averaging flags")
 
-		if not method: method = "linear"
+		if not method:
+			method = "linear"
 
 		# If time series agree, no interpolation is necessary.
 		if len(inp_time) == len(ref_time) and all(inp_time == ref_time):
 			outdata = inp_data
-			coords  = [ref.coords["time"]]
+			coords = [ref.coords["time"]]
 
 			if len(inp.coords) > 1:
 				for k in inp.dims[1:]:
@@ -268,8 +269,8 @@ def resample(inp=None,ref=None,**kwargs):
 			
 			return out
 
-		tck     = interpolate.interp1d(inp_time, inp_data, kind=method, axis=0, fill_value="extrapolate")
-		outdata = tck(ref_time)
+		tck = interpolate.interp1d(inp_time, inp_data, kind=method, axis=0, fill_value="extrapolate")
+		out_data = tck(ref_time)
 
 	coords = [ref.coords["time"]]
 
@@ -277,6 +278,6 @@ def resample(inp=None,ref=None,**kwargs):
 		for k in inp.dims[1:]:
 			coords.append(inp.coords[k])
 
-	out = xr.DataArray(outdata, coords=coords, dims=inp.dims,attrs=inp.attrs)
+	out = xr.DataArray(out_data, coords=coords, dims=inp.dims,attrs=inp.attrs)
 	
 	return out
