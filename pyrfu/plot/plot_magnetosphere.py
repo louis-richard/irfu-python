a7ac43501f9f063d2438fa72360a24c581ca517c
louis-richard <louisr@irfu.se>
2021-07-03 20:02:54 +0200

    Add option to plot field lines using GEOPACK

--- pyrfu/plot/plot_magnetosphere.py
+++ pyrfu/plot/plot_magnetosphere.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Built-in imports
+import logging
+
 # 3rd party imports
 import numpy as np
-
+from geopack import geopack
 from matplotlib.patches import Wedge
 
 # Local imports
-from ..pyrf import magnetosphere
+from ..pyrf import magnetosphere, datetime642unix, iso86012datetime64
 
 __author__ = "Louis Richard"
 __email__ = "louisr@irfu.se"
@@ -15,6 +18,11 @@ __copyright__ = "Copyright 2020-2021"
 __license__ = "MIT"
 __version__ = "2.3.7"
 __status__ = "Prototype"
+
+logging.captureWarnings(True)
+logging.basicConfig(format='%(asctime)s: %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S',
+                    level=logging.INFO)
 
 
 def _add_earth(ax=None, **kwargs):
@@ -26,7 +34,46 @@ def _add_earth(ax=None, **kwargs):
     return [nightside_, dayside_]
 
 
-def plot_magnetosphere(ax, tint, colors: list = None):
+def _add_field_lines(ax, tint):
+    # Get dipole axis at begin of the time interval
+    ut = datetime642unix(iso86012datetime64(np.array(tint)))[0]
+    ps = geopack.recalc(ut)
+
+    x_lines_m, y_lines_m, z_lines_m = [[], [], []]
+    x_lines_p, y_lines_p, z_lines_p = [[], [], []]
+
+    xx_gsm, zz_gsm = np.meshgrid(np.linspace(-30, 6, 19),
+                                 np.linspace(-5, 5, 10))
+    xx_gsm = np.reshape(xx_gsm, (xx_gsm.size,))
+    zz_gsm = np.reshape(zz_gsm, (zz_gsm.size,))
+
+    for x_gsm, z_gsm in zip(xx_gsm, zz_gsm):
+        y_gsm = 0
+        _, _, _, xx, yy, zz = geopack.trace(x_gsm, y_gsm, z_gsm, dir=-1,
+                                            rlim=100,
+                                            r0=.99999, parmod=2, exname='t89',
+                                            inname='igrf', maxloop=10000)
+        x_lines_m.append(xx)
+        z_lines_m.append(zz)
+
+        _, _, _, xx, yy, zz = geopack.trace(x_gsm, y_gsm, z_gsm, dir=1,
+                                            rlim=100,
+                                            r0=.99999, parmod=2, exname='t89',
+                                            inname='igrf', maxloop=10000)
+        x_lines_p.append(xx)
+        z_lines_p.append(zz)
+
+    for xx, zz in zip(x_lines_m, z_lines_m):
+        ax.plot(xx, zz, color="k", linewidth=.2)
+
+    for xx, zz in zip(x_lines_p, z_lines_p):
+        ax.plot(xx, zz, color="k", linewidth=.2)
+
+    return
+
+
+def plot_magnetosphere(ax, tint, colors: list = None,
+                       field_lines: bool = True):
     r"""Plot magnetopause, bow shock and earth.
 
     Parameters
@@ -48,7 +95,7 @@ def plot_magnetosphere(ax, tint, colors: list = None):
 
     # Compute Magnetopause
     if colors is None:
-        colors = ["tab:blue", "tab:red"]
+        colors = ["tab:blue", "tab:green"]
 
     x_mp, y_mp = magnetosphere("mp_shue1998", tint)
 
@@ -63,5 +110,9 @@ def plot_magnetosphere(ax, tint, colors: list = None):
             np.hstack([y_bs, np.flip(-y_bs)]),
             color=colors[1], label="Bow Shock")
     _add_earth(ax)
+
+    if field_lines:
+        logging.info("Computing field lines using T89 model...")
+        _add_field_lines(ax, tint)
 
     return ax
