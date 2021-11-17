02682ba2f29a9691556be059c7d1b0fd648b4374
louis-richard <louisr@irfu.se>
2021-11-17 09:28:08 +0100

    fix spin sector handling

--- pyrfu/mms/eis_spin_avg.py
+++ pyrfu/mms/eis_spin_avg.py
@@ -16,6 +16,24 @@ __version__ = "2.3.7"
 __status__ = "Prototype"
 
 
+def _check_spin(spin):
+    _, spin_inds = np.unique(spin, return_index=True)
+
+    len_spin = np.max(np.diff(spin_inds))
+
+    # Check to see if we have a complete first spin,
+    # if not then go to the second spin
+    if np.where(spin == spin[spin_inds[0]])[0].size != len_spin:
+        spin_inds = spin_inds[1:]
+
+    # Check to see if the last one is complete,
+    # if not then go to the second spin
+    if np.where(spin == spin[spin_inds[-1]])[0].size != len_spin:
+        spin_inds = spin_inds[:-1]
+
+    return spin_inds, len_spin
+
+
 def eis_spin_avg(eis_allt, method: str = "mean"):
     r"""Calculates spin-averaged fluxes for the EIS instrument.
 
@@ -55,7 +73,7 @@ def eis_spin_avg(eis_allt, method: str = "mean"):
 
     spin_nums = eis_allt.spin
 
-    spin_starts = np.where(spin_nums.data[1:] > spin_nums.data[:-1])[0]
+    spin_starts, len_spin = _check_spin(spin_nums.data)
 
     scopes = list(filter(lambda x: x[0] == "t", eis_allt))
 
@@ -64,23 +82,20 @@ def eis_spin_avg(eis_allt, method: str = "mean"):
     for scope in scopes:
         scope_data = eis_allt[scope]
 
-        time_recs = scope_data.time.data[spin_starts]
+        time_recs = scope_data.time.data[spin_starts + len_spin // 2]
         energies_ = scope_data.energy.data
         flux_data = scope_data.data
 
         flux_av = np.zeros([len(spin_starts), len(energies_)])
         flux_su = np.zeros([len(spin_starts), len(energies_)])
 
-        c_strt = 0
-
         for i, spin_strt in enumerate(spin_starts):
+            t_inds = np.where(spin_nums.data == spin_nums.data[spin_strt])[0]
+
             with warnings.catch_warnings():
                 warnings.simplefilter("ignore", category=RuntimeWarning)
-                flux_av[i, :] = np.nanmean(flux_data[c_strt:spin_strt + 1, :],
-                                           axis=0)
-                flux_su[i, :] = np.nansum(flux_data[c_strt:spin_strt + 1,
-                                          :], axis=0)
-            c_strt = spin_strt + 1
+                flux_av[i, :] = np.nanmean(flux_data[t_inds, :], axis=0)
+                flux_su[i, :] = np.nansum(flux_data[t_inds, :], axis=0)
 
         spin_avg_flux[scope] = xr.DataArray(flux_av,
                                             coords=[time_recs, energies_],
@@ -89,12 +104,12 @@ def eis_spin_avg(eis_allt, method: str = "mean"):
                                             coords=[time_recs, energies_],
                                             dims=["time", "energy"])
 
-    spin_avg_flux["spin"] = eis_allt["spin"][spin_starts]
+    spin_avg_flux["spin"] = eis_allt["spin"][spin_starts + len_spin // 2]
     spin_avg_flux["energy_dplus"] = eis_allt.energy_dplus.data
     spin_avg_flux["energy_dminus"] = eis_allt.energy_dminus.data
     spin_avg_flux = xr.Dataset(spin_avg_flux, attrs=eis_allt.attrs)
 
-    spin_sum_flux["spin"] = eis_allt["spin"][spin_starts]
+    spin_sum_flux["spin"] = eis_allt["spin"][spin_starts + len_spin // 2]
     spin_sum_flux["energy_dplus"] = eis_allt.energy_dplus.data
     spin_sum_flux["energy_dminus"] = eis_allt.energy_dminus.data
     spin_sum_flux = xr.Dataset(spin_sum_flux, attrs=eis_allt.attrs)
