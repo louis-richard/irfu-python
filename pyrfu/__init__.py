0e7316337fd6f8ab56fc565ebbc3bd64d8751a8f
louis-richard <louisr@irfu.se>
2021-12-09 09:50:43 +0100

    v2.3.8

--- pyrfu/__init__.py
+++ pyrfu/__init__.py
@@ -5,11 +5,11 @@ import unittest
 import numpy as np
 
 
-from pyrfu import mms, pyrf, models, dispersion, pydrk
+from pyrfu import mms, pyrf, models, dispersion
 
 __author__ = "Louis Richard"
 __email__ = "louisr@irfu.se"
 __copyright__ = "Copyright 2020-2021"
 __license__ = "MIT"
-__version__ = "2.3.7"
+__version__ = "2.3.8"
 __status__ = "Prototype"
