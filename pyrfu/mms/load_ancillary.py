c8081dcc9a26d3271b72d0edbfa7a2b95de464d3
louis-richard <louisr@irfu.se>
2022-08-16 12:34:00 +0200

    rename variable level_and_dtype to product and use external list_files_ancillary.py

--- pyrfu/mms/load_ancillary.py
+++ pyrfu/mms/load_ancillary.py
@@ -5,16 +5,16 @@
 import os
 import re
 import json
-import glob
 import bisect
-import fnmatch
 import logging
 
 # 3rd party imports
 import pandas as pd
 
-# Local imports
 from ..pyrf import iso86012datetime
+
+# Local imports
+from .list_files_ancillary import list_files_ancillary
 
 __author__ = "Louis Richard"
 __email__ = "louisr@irfu.se"
@@ -29,14 +29,12 @@ logging.basicConfig(
 )
 
 
-def load_ancillary(
-    level_and_dtype, tint, mms_id, verbose: bool = True, data_path: str = ""
-):
+def load_ancillary(product, tint, mms_id, verbose: bool = True, data_path: str = ""):
     r"""Loads ancillary data.
 
     Parameters
     ----------
-    level_and_dtype : {"predatt", "predeph", "defatt", "defeph"}
+    product : {"predatt", "predeph", "defatt", "defeph"}
         Ancillary type.
     tint : list of str
         Time interval
@@ -54,72 +52,11 @@ def load_ancillary(
 
     """
 
-    # Check path
-    if not data_path:
-        pkg_path = os.path.dirname(os.path.abspath(__file__))
+    # Get path of files in interval
+    files_names = list_files_ancillary(tint, mms_id, product, data_path)
 
-        # Read the current version of the MMS configuration file
-        with open(os.path.join(pkg_path, "config.json"), "r") as fs:
-            config = json.load(fs)
-
-        data_path = os.path.normpath(config["local_data_dir"])
-    else:
-        data_path = os.path.normpath(data_path)
-
-    # Make sure that the data path exists
-    assert os.path.exists(data_path), f"{data_path} doesn't exist!!"
-
-    if isinstance(mms_id, int):
-        mms_id = str(mms_id)
-
+    # Convert time interval to datetime
     tint = iso86012datetime(tint)
-
-    # directory and file name search patterns
-    # For now
-    # -all ancillary data is in one directory:
-    #       mms\ancillary
-    # -assume file names are of the form:
-    #   SPACECRAFT_FILETYPE_startDate_endDate.version
-    #   where SPACECRAFT is [MMS1, MMS2, MMS3, MMS4] in uppercase
-    #   and FILETYPE is either DEFATT, PREDATT, DEFEPH, PREDEPH in uppercase
-    #   and start/endDate is YYYYDOY
-    #   and version is Vnn (.V00, .V01, etc..)
-    dir_pattern = os.sep.join([data_path, "ancillary", f"mms{mms_id}", level_and_dtype])
-    file_pattern = "_".join(
-        ["MMS{}".format(mms_id), level_and_dtype.upper(), "???????_???????.V??"]
-    )
-
-    files_in_tint = []
-    out_files = []
-
-    files = glob.glob(os.sep.join([dir_pattern, file_pattern]))
-
-    # find the files within the time interval
-    fname_fmt = (
-        f"MMS{mms_id}_{level_and_dtype.upper()}"
-        f"_([0-9]{{7}})_([0-9]{{7}}).V[0-9]{{2}}"
-    )
-    file_regex = re.compile(os.sep.join([dir_pattern, fname_fmt]))
-    for file in files:
-        time_match = file_regex.match(file)
-        if time_match is not None:
-            start_time = pd.to_datetime(time_match.group(1), format="%Y%j")
-            end_time = pd.to_datetime(time_match.group(2), format="%Y%j")
-            if start_time < tint[1] and end_time >= tint[0]:
-                files_in_tint.append(file)
-
-    # ensure only the latest version of each file is loaded
-    for file in files_in_tint:
-        this_file = file[0:-3] + "V??"
-        versions = fnmatch.filter(files_in_tint, this_file)
-        if len(versions) > 1:
-            # only grab the latest version
-            out_files.append(sorted(versions)[-1])
-        else:
-            out_files.append(versions[0])
-
-    files_names = list(set(out_files))
-    files_names.sort()
 
     # Read length of header and columns names from .json file
     # Root path
@@ -129,7 +66,7 @@ def load_ancillary(
         anc_dict = json.load(file)
 
     if verbose:
-        logging.info("Loading ancillary %s files...", level_and_dtype)
+        logging.info("Loading ancillary %s files...", product)
 
     data_frame_dict = {}
 
@@ -138,19 +75,19 @@ def load_ancillary(
             file,
             delim_whitespace=True,
             header=None,
-            skiprows=anc_dict[level_and_dtype]["header"],
+            skiprows=anc_dict[product]["header"],
         )
 
         # Remove footer
         rows = rows[:][:-1]
 
         # Convert time
-        fmt = anc_dict[level_and_dtype]["time_format"]
+        fmt = anc_dict[product]["time_format"]
         rows[0] = pd.to_datetime(rows[0], format=fmt)
 
         start_idx = bisect.bisect_left(rows[0][:], tint[0])
         end_idx = bisect.bisect_left(rows[0][:], tint[1])
-        rows.columns = anc_dict[level_and_dtype]["columns_names"]
+        rows.columns = anc_dict[product]["columns_names"]
 
         data_frame_dict[i] = rows[:][start_idx:end_idx]
 
