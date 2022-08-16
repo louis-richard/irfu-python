a6520ce6a7b0eec55696afa93c811eb441e19895
louis-richard <louisr@irfu.se>
2022-08-16 12:38:09 +0200

    add new routines to __init__.py

--- pyrfu/solo/__init__.py
+++ pyrfu/solo/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from .db_init import db_init
 from .read_tnr import read_tnr
+from .read_lfr_density import read_lfr_density
 
 __author__ = "Louis Richard"
 __email__ = "louisr@irfu.se"
