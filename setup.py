3315f58fc2b2001ef57fadef536d3e7145bfb84d
louis-richard <louisr@irfu.se>
2022-08-16 12:40:58 +0200

    v2.3.22

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ __author__ = "Louis Richard"
 __email__ = "louisr@irfu.se"
 __copyright__ = "Copyright 2020-2022"
 __license__ = "MIT"
-__version__ = "2.3.21"
+__version__ = "2.3.22"
 __status__ = "Prototype"
 
 HERE = pathlib.Path(__file__).parent
