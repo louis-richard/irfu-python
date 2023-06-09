9bee537de42db55ecaf7333040802553e3af7e69
louis-richard <louisr@irfu.se>
2023-06-09 17:59:43 +0200

    Bump version 2.3.35 -> 2.4.0

--- pyrfu/__init__.py
+++ pyrfu/__init__.py
@@ -7,7 +7,7 @@ __author__ = "Louis Richard"
 __email__ = "louisr@irfu.se"
 __copyright__ = "Copyright 2020-2021"
 __license__ = "MIT"
-__version__ = "2.3.35"
+__version__ = "2.4.0"
 __status__ = "Prototype"
 
 __all__ = ["dispersion", "lp", "mms", "models", "plot", "pyrf"]
