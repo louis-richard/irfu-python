4077dfbbaa9f6c75fbfbd250485d9120cf9cb199
louis-richard <louisr@irfu.se>
2024-02-21 18:32:19 +0100

    enable choosing the reference time in generate_timeline(), and handle higher order tensors in generate_data() and generate_ts()

--- pyrfu/tests/__init__.py
+++ pyrfu/tests/__init__.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import numpy as np
+# Built-in imports
+import string
 
-from pyrfu import pyrf
+# 3rd party imports
+import numpy as np
+import xarray as xr
+
+# Local imports
+from .. import pyrf
 
 __author__ = "Louis Richard"
 __email__ = "louisr@irfu.se"
@@ -22,7 +28,12 @@ __all__ = [
 ]
 
 
-def generate_timeline(f_s, n_pts: int = 10000, dtype="datetime64[ns]"):
+def generate_timeline(
+    f_s,
+    n_pts: int = 10000,
+    dtype="datetime64[ns]",
+    ref_time: str = "2019-01-01T00:00:00.000000000",
+):
     r"""Generate timeline for testings
 
     Parameters
@@ -30,49 +41,44 @@ def generate_timeline(f_s, n_pts: int = 10000, dtype="datetime64[ns]"):
     f_s
     n_pts
     dtype
+    ref_time
 
     Returns
     -------
 
     """
-    ref_time = np.datetime64("2019-01-01T00:00:00.000000000")
+    ref_time = np.datetime64(ref_time)
     times = [ref_time + np.timedelta64(int(i * 1e9 / f_s), "ns") for i in range(n_pts)]
     times = np.array(times).astype(dtype)
     return times
 
 
-def generate_data(n_pts, kind: str = "scalar"):
+def generate_data(n_pts, tensor_order: int = 0):
     r"""Generate data (numpy.ndarray) for testings
 
     Parameters
     ----------
     n_pts
-    kind
+    tensor_order
 
     Returns
     -------
-
+    data
     """
-    if kind == "scalar":
-        data = np.random.random((n_pts,))
-    elif kind == "vector":
-        data = np.random.random((n_pts, 3))
-    elif kind == "tensor":
-        data = np.random.random((n_pts, 3, 3))
-    else:
-        raise ValueError("Invalid kind of data!!")
+
+    data = np.random.random((n_pts, *([3] * tensor_order)))
 
     return data
 
 
-def generate_ts(f_s, n_pts, kind: str = "scalar", attrs=None):
+def generate_ts(f_s, n_pts, tensor_order: int = 0, attrs: dict = None):
     r"""Generate timeseries for testings
 
     Parameters
     ----------
     f_s
     n_pts
-    kind
+    tensor_order
     attrs
 
     Returns
@@ -82,20 +88,21 @@ def generate_ts(f_s, n_pts, kind: str = "scalar", attrs=None):
     if attrs is None:
         attrs = {}
 
-    if kind == "scalar":
-        out = pyrf.ts_scalar(
-            generate_timeline(f_s, n_pts), generate_data(n_pts, kind), attrs=attrs
-        )
-    elif kind == "vector":
-        out = pyrf.ts_vec_xyz(
-            generate_timeline(f_s, n_pts), generate_data(n_pts, kind), attrs=attrs
-        )
-    elif kind == "tensor":
-        out = pyrf.ts_tensor_xyz(
-            generate_timeline(f_s, n_pts), generate_data(n_pts, kind), attrs=attrs
-        )
+    time = generate_timeline(f_s, n_pts)
+    data = generate_data(n_pts, tensor_order=tensor_order)
+
+    if tensor_order == 0:
+        out = pyrf.ts_scalar(time, data, attrs=attrs)
+    elif tensor_order == 1:
+        out = pyrf.ts_vec_xyz(time, data, attrs=attrs)
+    elif tensor_order == 2:
+        out = pyrf.ts_tensor_xyz(time, data, attrs=attrs)
     else:
-        raise ValueError("Invalid kind of data!!")
+        coords = [time, *[np.arange(data.shape[i]) for i in range(1, tensor_order + 1)]]
+        dims = ["time", *list(string.ascii_lowercase[8 : 8 + tensor_order])]
+        out = xr.DataArray(data, coords=coords, dims=dims, attrs=attrs)
+
+    out.time.attrs = {"UNITS": "I AM GROOT!!"}
 
     return out
 
