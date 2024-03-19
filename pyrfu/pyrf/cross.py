06dfb98943127e1c9dbf02208fb76be600de3bed
louis-richard <louisr@irfu.se>
2024-03-19 19:42:25 +0100

    fix mypy code errors

--- pyrfu/pyrf/cross.py
+++ pyrfu/pyrf/cross.py
@@ -3,34 +3,41 @@
 
 # 3rd party imports
 import numpy as np
-import xarray as xr
+from xarray.core.dataarray import DataArray
 
 # Local imports
-from .resample import resample
-from .ts_vec_xyz import ts_vec_xyz
+from pyrfu.pyrf.resample import resample
+from pyrfu.pyrf.ts_vec_xyz import ts_vec_xyz
 
 __author__ = "Louis Richard"
 __email__ = "louisr@irfu.se"
-__copyright__ = "Copyright 2020-2023"
+__copyright__ = "Copyright 2020-2024"
 __license__ = "MIT"
-__version__ = "2.4.2"
+__version__ = "2.4.13"
 __status__ = "Prototype"
 
 
-def cross(inp1, inp2):
-    r"""Computes cross product of two fields.
+def cross(inp1: DataArray, inp2: DataArray) -> DataArray:
+    r"""Compute cross product of two fields.
 
     Parameters
     ----------
-    inp1 : xarray.DataArray
+    inp1 : DataArray
         Time series of the first field X.
-    inp2 : xarray.DataArray
+    inp2 : DataArray
         Time series of the second field Y.
 
     Returns
     -------
-    out : xarray.DataArray
+    DataArray
         Time series of the cross product Z = XxY.
+
+    Raises
+    ------
+    TypeError
+        If inp1 or inp2 are not xarray.DataArray.
+    ValueError
+        If inp1 or inp2 are not vectors timeseries.
 
     Examples
     --------
@@ -46,8 +53,8 @@ def cross(inp1, inp2):
 
     Load magnetic field and electric field
 
-    >>> b_xyz = mms.get_data("B_gse_fgm_srvy_l2", tint, mms_id)
-    >>> e_xyz = mms.get_data("E_gse_edp_fast_l2", tint, mms_id)
+    >>> b_xyz = mms.get_data("b_gse_fgm_srvy_l2", tint, mms_id)
+    >>> e_xyz = mms.get_data("e_gse_edp_fast_l2", tint, mms_id)
 
     Compute magnitude of the magnetic field
 
@@ -58,14 +65,19 @@ def cross(inp1, inp2):
     >>> v_xyz_exb = pyrf.cross(e_xyz, b_xyz) / b_mag ** 2
 
     """
-
     # Check type
-    assert isinstance(inp1, xr.DataArray), "inp1 must be a xarray.DataArray"
-    assert isinstance(inp2, xr.DataArray), "inp2 must be a xarray.DataArray"
+    if not isinstance(inp1, DataArray):
+        raise TypeError("inp1 must be a xarray.DataArray")
+
+    if not isinstance(inp2, DataArray):
+        raise TypeError("inp2 must be a xarray.DataArray")
 
     # Check inputs are vectors
-    assert inp1.ndim == 2 and inp1.shape[1] == 3, "inp1 must be a vector"
-    assert inp2.ndim == 2 and inp2.shape[1] == 3, "inp1 must be a vector"
+    if inp1.ndim != 2 or inp1.shape[1] != 3:
+        raise ValueError("inp1 must be a vector")
+
+    if inp2.ndim != 2 or inp2.shape[1] != 3:
+        raise ValueError("inp2 must be a vector")
 
     if len(inp1) != len(inp2):
         inp2 = resample(inp2, inp1)
--- pyrfu/pyrf/ts_vec_xyz.py
+++ pyrfu/pyrf/ts_vec_xyz.py
@@ -1,47 +1,74 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Built-in imports
+from typing import Any, Mapping, Optional
+
 # 3rd party imports
 import numpy as np
 import xarray as xr
+from numpy.typing import NDArray
+from xarray.core.dataarray import DataArray
 
 __author__ = "Louis Richard"
 __email__ = "louisr@irfu.se"
-__copyright__ = "Copyright 2020-2023"
+__copyright__ = "Copyright 2020-2024"
 __license__ = "MIT"
-__version__ = "2.4.2"
+__version__ = "2.4.13"
 __status__ = "Prototype"
 
 
-def ts_vec_xyz(time, data, attrs: dict = None):
+def ts_vec_xyz(
+    time: NDArray[np.datetime64],
+    data: NDArray[Any],
+    attrs: Optional[Mapping[str, object]] = None,
+) -> DataArray:
     r"""Create a time series containing a 1st order tensor.
 
     Parameters
     ----------
-    time : ndarray
+    time : numpy.ndarray
         Array of times.
-    data : ndarray
+    data : numpy.ndarray
         Data corresponding to the time list.
     attrs : dict, Optional
         Attributes of the data list.
 
     Returns
     -------
-    out : xarray.DataArray
+    out : DataArray
         1st order tensor time series.
 
-    """
+    Raises
+    ------
+    TypeError
+        If time or data is not a numpy.ndarray or if attrs is not a dict.
+    ValueError
+        If data does not have shape (n, 3) or if time and data do not have the same
+        length.
 
+    """
     # Check input type
-    assert isinstance(time, np.ndarray), "time must be a numpy.ndarray"
-    assert isinstance(data, np.ndarray), "data must be a numpy.ndarray"
+    if not isinstance(time, np.ndarray):
+        raise TypeError("time must be a numpy.ndarray")
+
+    if not isinstance(data, np.ndarray):
+        raise TypeError("data must be a numpy.ndarray")
 
     # Check input shape must be (n, 3)
-    assert data.ndim == 2 and data.shape[1] == 3
-    assert len(time) == len(data), "Time and data must have the same length"
+    if data.ndim != 2 or data.shape[1] != 3:
+        raise ValueError("data must have shape (n, 3)")
 
-    if attrs is None or not isinstance(attrs, dict):
-        attrs = {}
+    # Check input length
+    if len(time) != len(data):
+        raise ValueError("Time and data must have the same length")
+
+    if attrs is None:
+        attrs = {"TENSOR_ORDER": 1}
+    elif isinstance(attrs, dict):
+        attrs["TENSOR_ORDER"] = 1
+    else:
+        raise TypeError("attrs must be a dict")
 
     out = xr.DataArray(
         data,
@@ -49,7 +76,5 @@ def ts_vec_xyz(time, data, attrs: dict = None):
         dims=["time", "comp"],
         attrs=attrs,
     )
-
-    out.attrs["TENSOR_ORDER"] = 1
 
     return out
