dd1a616d30f814fd34be4ce20edf9c9645582fbd
louis-richard <louisr@irfu.se>
2024-07-30 16:44:19 +0200

    add unittests and static typing

--- pyrfu/pyrf/ts_tensor_xyz.py
+++ pyrfu/pyrf/ts_tensor_xyz.py
@@ -1,44 +1,68 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Built-in imports
+from typing import Mapping, Optional, Union
+
 # 3rd party imports
 import numpy as np
 import xarray as xr
+from numpy.typing import NDArray
+from xarray.core.dataarray import DataArray
 
 __author__ = "Louis Richard"
 __email__ = "louisr@irfu.se"
-__copyright__ = "Copyright 2020-2023"
+__copyright__ = "Copyright 2020-2024"
 __license__ = "MIT"
-__version__ = "2.4.2"
+__version__ = "2.4.13"
 __status__ = "Prototype"
 
 
-def ts_tensor_xyz(time, data, attrs: dict = None):
+def ts_tensor_xyz(
+    time: NDArray[np.datetime64],
+    data: NDArray[Union[np.float32, np.float64]],
+    attrs: Optional[Mapping[str, object]] = None,
+) -> DataArray:
     r"""Create a time series containing a 2nd order tensor.
 
     Parameters
     ----------
-    time : ndarray
+    time : numpy.ndarray
         Array of times.
-    data : ndarray
+    data : numpy.ndarray
         Data corresponding to the time list.
     attrs : dict, Optional
         Attributes of the data list.
 
     Returns
     -------
-    out : xarray.DataArray
+    DataArray
         2nd order tensor time series.
+
+    Raises
+    ------
+    TypeError
+        If time or data is not a numpy.ndarray.
+    ValueError
+        * If time and data do not have the same length.
+        * If data does not have shape (n, 3, 3).
 
     """
 
     # Check input type
-    assert isinstance(time, np.ndarray), "time must be a numpy.ndarray"
-    assert isinstance(data, np.ndarray), "data must be a numpy.ndarray"
+    if not isinstance(time, np.ndarray):
+        raise TypeError("time must be a numpy.ndarray")
+
+    if not isinstance(data, np.ndarray):
+        raise TypeError("data must be a numpy.ndarray")
+
+    # Check data and time have the same length
+    if len(time) != len(data):
+        raise ValueError("Time and data must have the same length")
 
     # Check input shape must be (n, 3, 3)
-    assert data.ndim == 3 and data.shape[1:] == (3, 3)
-    assert len(time) == len(data), "Time and data must have the same length"
+    if data.ndim != 3 or data.shape[1:] != (3, 3):
+        raise ValueError("data must have shape (n, 3, 3)")
 
     if attrs is None or not isinstance(attrs, dict):
         attrs = {}
@@ -46,7 +70,7 @@ def ts_tensor_xyz(time, data, attrs: dict = None):
     out = xr.DataArray(
         data,
         coords=[time[:], ["x", "y", "z"], ["x", "y", "z"]],
-        dims=["time", "comp_h", "comp_v"],
+        dims=["time", "rcomp", "ccomp"],
         attrs=attrs,
     )
 
