7eb865a50e215bc549d85f1a243c81175c52d593
Apostolos Kolokotronis <apostolosk@irf.se>
2026-02-02 09:18:46 +0100

    debug wavelet.py for fk_power_spectrum_4sc.py

--- pyrfu/pyrf/wavelet.py
+++ pyrfu/pyrf/wavelet.py
@@ -16,8 +16,6 @@ from xarray.core.dataarray import DataArray
 from xarray.core.dataset import Dataset
 
 # Local imports
-from .calc_fs import calc_fs
-
 __author__ = "Louis Richard"
 __email__ = "louisr@irfu.se"
 __copyright__ = "Copyright 2020-2024"
@@ -122,7 +120,7 @@ def wavelet(
         raise TypeError("Input must be a DataArray")
 
     if f_s is None:
-        f_s = calc_fs(inp)
+        f_s = pyrf.calc_fs(inp)
 
     if n_freqs is None:
         n_freqs = 200
@@ -148,6 +146,7 @@ def wavelet(
     f_nyq: float = f_s / 2
     sigma: float = wavelet_width / f_nyq
 
+
     # Frequency range
     if f is None:
         f_min: float = f_nyq / 10**2
@@ -156,19 +155,24 @@ def wavelet(
         f_min, f_max = sorted(f)
 
     if linear_df:
-        scale_number: int = int(np.floor(f_nyq / delta_f))
-
+        
+        f_min: float = delta_f
+        scale_number: int = int(np.floor(f_nyq / delta_f,))
+        f_max = scale_number * delta_f
         # Scales range
         scale_min: float = delta_f
         scale_max: float = scale_number * delta_f
         scales: NDArray[np.float64] = f_nyq / (
-            np.linspace(scale_max, scale_min, scale_number, dtype=np.float64)
+            np.linspace(f_max, f_min, scale_number, dtype=np.float64)
         )
+        # return f_min, f_max, scale_number, scale_min, scale_max, scales, f_nyq
     else:
         scale_number = n_freqs
         scale_min = np.log10(f_nyq / f_max)
         scale_max = np.log10(f_nyq / f_min)
         scales = np.logspace(scale_min, scale_max, scale_number, dtype=np.float64)
+
+
 
     # Unpack time and data.
     # Remove the last sample if the total number of samples is odd.
@@ -186,7 +190,7 @@ def wavelet(
         )
     else:
         power2 = np.zeros((len(time), n_freqs), dtype=np.complex128)
-
+    
     # Check for NaNs
     scales[np.isnan(scales)] = 0.0
 
@@ -219,6 +223,7 @@ def wavelet(
 
         # Wavelet transform of the data
         # Forward FFT
+        # s_w: NDArray[np.complex128] = np.fft.fft(data_col,)
         s_w: NDArray[np.complex128] = fft.fft(data_col, workers=os.cpu_count())
 
         scales_mat, s_w_mat = np.meshgrid(scales, s_w, sparse=True)
@@ -251,17 +256,19 @@ def wavelet(
                 ["time", "frequency"],
                 np.fliplr(power2),
             )
+    # return power2, freqs_cwt
 
     if len(inp.shape) == 1:
         out: Union[DataArray, Dataset] = xr.DataArray(
             power2,
-            coords=[time, np.flip(freqs_cwt)],
+            coords=[time, freqs_cwt],
             dims=["time", "frequency"],
         )
+        # return out, power2, freqs_cwt
     else:
         out = xr.Dataset(
             out_dict,
             coords={"time": time, "frequency": np.flip(freqs_cwt)},
         )
-
+    # return power2, freqs_cwt
     return out
