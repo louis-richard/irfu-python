7b9ef8c835860d7f6fe779bbe0c6c41e435d71b8
Louis Richard <louis.richard@irf.se>
2026-02-05 16:59:59 +0100

    change time precision for interpolation

--- pyrfu/pyrf/resample.py
+++ pyrfu/pyrf/resample.py
@@ -124,8 +124,10 @@ def _resample_dataarray(inp, ref, method, f_s, window, thresh, verbose=False):
     else:
         sfy = None
 
-    inp_time = inp.time.data.view("i8") * 1e-9
-    ref_time = ref.time.data.view("i8") * 1e-9
+    inp_time_ttns = inp.time.data.astype("int64")
+    ref_time_ttns = ref.time.data.astype("int64")
+    inp_time = (inp_time_ttns - inp_time_ttns[0]) * 1e-9
+    ref_time = (ref_time_ttns - inp_time_ttns[0]) * 1e-9
 
     if flag_do == "check":
         if len(ref_time) > 1:
